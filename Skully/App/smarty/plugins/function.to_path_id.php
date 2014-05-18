df372142b096f6ec67ce8ec0c26229a74f352cb3
Jay Teguh <jay@tgitriodesign.com>
2014-05-18 09:38:58 +0700

    fixes on UrlHelper

--- Skully/App/smarty/plugins/function.to_path_id.php
+++ Skully/App/smarty/plugins/function.to_path_id.php
@@ -1,6 +1,6 @@
 <?php
 
 function smarty_function_to_path_id($params = array(), &$smarty) {
-    $pathId = \App\Helpers\UrlHelper::toPathId($params['name'], $params['id']);
+    $pathId = \Skully\App\Helpers\UrlHelper::toPathId($params['name'], $params['id']);
     return $pathId;
 }
