31389c917a504068441a51e0d77501e62e4b6feb
Bobby Stenly Irawan <bobby@tgitriodesign.com>
2015-03-19 16:37:33 +0700

    Fixing Get Url without SSL bugs

--- Skully/App/smarty/plugins/function.url.php
+++ Skully/App/smarty/plugins/function.url.php
@@ -20,7 +20,7 @@ function smarty_function_url($params = array(), &$smarty) {
 		unset($params['path']);
 	}
 
-    if (!isset($params['__ssl'])) {
+    if (isset($params['__ssl'])) {
         $ssl = $params['__ssl'];
         unset($params['__ssl']);
     }
